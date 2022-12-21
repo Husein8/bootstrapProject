06c95e32c7efcf4df407fde6c5beddb53fa8c84c
husein <softichusein8@gmail.com>
2022-12-21 15:31:32 +0800

    files

---
+++ js.js
@@ -0,0 +1,44 @@
+// var numberOfBottles = prompt("enter how many bottles u want to buy");
+
+// function getMilk(money) {   
+//     var totalNumber = Math.floor(money / 1.5);
+//     console.log("leaveHouse");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log(`u can buy ${totalNumber} bottles`);
+//     console.log("moveRight");
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");    
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("enterHouse");
+//   }
+
+//   getMilk(numberOfBottles)
+
+// function getMilk2(bottles){
+//     let price = bottles * 2;
+//     console.log(`the price is ${price} dollars`);
+// }
+
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        var yearDays = age * 365;
+        var yearWeeks = age * 48;
+        var yearMonths = age * 30;
+        console.log(`you have ${yearDays} days and ${yearWeeks} weeks, and ${yearMonths} months`);
+        //Write your code here.
+   
+    /*************Don't change the code below**********/
+    }
+    lifeInWeeks(56)
+
